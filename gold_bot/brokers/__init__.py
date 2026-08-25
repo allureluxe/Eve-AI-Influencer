32a5dab52a20757251d09e72e71a25cf8499a4cd
allureluxe <152798851+allureluxe@users.noreply.github.com>
2026-08-25 23:15:34 +0200

    refactor: make Bitvavo the only broker export

--- gold_bot/brokers/__init__.py
+++ gold_bot/brokers/__init__.py
@@ -1,16 +1,5 @@
-"""Lieux d'execution disponibles."""
+"""Lieu d'execution unique : Bitvavo."""
 from .base import AccountInfo, Broker, BrokerError
-from .binance import BinanceBroker, BinanceConfig
-from .binance_spot import BinanceSpotBroker, SpotConfig
 from .bitvavo import BitvavoBroker, BitvavoConfig
-from .moonx import MoonXBroker, MoonXConfig
-from .okx import OkxBroker, OkxConfig
-from .paper import PaperBroker, PaperConfig
 
-__all__ = ["Broker", "BrokerError", "AccountInfo",
-           "PaperBroker", "PaperConfig",
-           "MoonXBroker", "MoonXConfig",
-           "BinanceBroker", "BinanceConfig",
-           "BinanceSpotBroker", "SpotConfig",
-           "BitvavoBroker", "BitvavoConfig",
-           "OkxBroker", "OkxConfig"]
+__all__ = ["Broker", "BrokerError", "AccountInfo", "BitvavoBroker", "BitvavoConfig"]
